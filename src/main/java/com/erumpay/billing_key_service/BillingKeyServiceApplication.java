e8b51cf6f5ed575b0e2e52a214a520a00004dae2
hajihyeok <jihyeok0502@gmail.com>
2026-05-13 16:54:04 +0900

    [KAN-1099] chore: setting

---
+++ src/main/java/com/erumpay/billing_key_service/BillingKeyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erumpay.billing_key_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingKeyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingKeyServiceApplication.class, args);
+	}
+
+}
